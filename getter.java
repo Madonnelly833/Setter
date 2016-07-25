47279851ee84527d68cf1c841142f9fdad421274
Madonnelly833 <mdon1@umbc.edu>
2016-07-25 16:36:41 -0400

    Setter

---
+++ getter.java
@@ -0,0 +1,136 @@
+package com.tiy.getter;
+
+public class getter
+{
+
+	private float kb
+
+	private float getKb() {
+		return kb;
+	}
+
+	private void setKb(float kb) {
+		this.kb = kb;
+	}
+
+	private	float acre
+
+	private float getAcre() {
+		return acre;
+	}
+
+	private void setAcre(float acre) {
+		this.acre = acre;
+	}
+
+	private String fileName
+
+	private String getFileName() {
+		return fileName;
+	}
+
+	private void setFileName(String fileName) {
+		this.fileName = fileName;
+
+	}
+
+	private	String phoneNumber
+
+	private String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	private void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	private	String companyName
+
+
+	private String getCompanyName() {
+		return companyName;
+	}
+
+	private void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	
+	private Date todayDate
+
+	private Date getTodayDate() {
+		return todayDate;
+	}
+
+	private void setTodayDate(Date todayDate) {
+		this.todayDate = todayDate;
+	}
+
+	private	boolean bulb
+
+	private boolean isBulb() {
+		return bulb;
+	}
+
+	private void setBulb(boolean bulb) {
+		this.bulb = bulb;
+	}
+
+	private	long pi
+
+	private long getPi() {
+		return pi;
+	}
+
+	private void setPi(long pi) {
+		this.pi = pi;
+	}
+
+	private	long lightSpeed
+
+	private long getLightSpeed() {
+		return lightSpeed;
+	}
+
+	private void setLightSpeed(long lightSpeed) {
+		this.lightSpeed = lightSpeed;
+	}
+
+	private Vehicle car
+
+	private Vehicle getCar() {
+		return car;
+	}
+
+	private void setCar(Vehicle car) {
+		this.car = car;
+	}
+
+	private Address Home
+
+	private Address getHome() {
+		return Home;
+	}
+
+	private void setHome(Address home) {
+		Home = home;
+	}
+
+
+public String Product[]
+
+private String[] getProduct() {
+	return Product;
+}
+
+private void setProduct(String[] product) {
+	Product = product;
+}
+
+
+
+
+
+
+
+
+}
